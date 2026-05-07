5cc2250d1285091920e12e06316a0d5e55460a03
kthydln5 <cathydalan5@gmail.com>
2026-05-07 08:00:00 +0800

    Connect C# GetApi method to api.php

--- CarRentalSimpleApp/Program.cs
+++ CarRentalSimpleApp/Program.cs
@@ -11,7 +11,6 @@ namespace CarRentalSimpleApp
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
-            
         }
     }
 }
