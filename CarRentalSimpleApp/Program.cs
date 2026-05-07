f429806273197050e5444f40df8e005d80633c15
kthydln5 <cathydalan5@gmail.com>
2026-05-07 09:16:00 +0800

    Load vehicles into C# DataGridView

--- CarRentalSimpleApp/Program.cs
+++ CarRentalSimpleApp/Program.cs
@@ -11,6 +11,7 @@ namespace CarRentalSimpleApp
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+
         }
     }
 }
